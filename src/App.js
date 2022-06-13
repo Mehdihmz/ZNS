25859e5ee65a66898024f8127acffd0135ebe37e
Mehdi <mehdihormozi22@gmail.com>
2022-06-13 18:29:54 +0300

    added nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const App = () => {
     <div className='App'>
         <h1>Hello World</h1>
         <p>Push my App to git !</p>
+        <nav>
+          <ul>
+            <li> Home </li>
+            <li> About </li>
+            <li> Contacts </li>
+          </ul>
+        </nav>
     </div>
   )
 }
