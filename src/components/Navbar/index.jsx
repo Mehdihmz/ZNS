a04bcf6376476866b25a5af120985b949488ee24
Mehdi <mehdihormozi22@gmail.com>
2022-06-18 10:52:09 +0300

    bootstrap for dropdown lang & button regstr

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,14 @@ function Navbar() {
     return (
         <div className="navbar-head">
             <div className="logo">
-                <img src="" alt="" />
+                <img src="/public/images/logo.svg" alt="" />
+            </div>
+            <div className="links-head">
+                <a className="link" href=""> Функции </a>
+                <a className="link" href=""> Контакты </a>
+                <a className="link" href=""> Вход </a>
+                <a className="link" href=""> Регистрация </a>
+                
             </div>
         </div>
     )
