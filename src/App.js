8dedf781372bdd88078c07dbc29b37cbcef160d4
Mehdi <mehdihormozi22@gmail.com>
2022-06-18 10:33:43 +0300

    create files for navbar & header

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import './App.css' ;
 const App = () => {
   return (
     <div className='App'>
-        <h1>Hello World</h1>
-        <p>Push my App to git !</p>
-        <nav>
-          <ul>
-            <li> Home </li>
-            <li> About </li>
-            <li> Contacts </li>
-          </ul>
-        </nav>
-        <footer>
-          Footer
-        </footer>
+       
     </div>
   )
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="navbar-head">
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
