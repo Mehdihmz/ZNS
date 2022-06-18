9bfa0533ff72c4d8cff751e697398b5accec068a
Mehdi <mehdihormozi22@gmail.com>
2022-06-18 11:45:45 +0300

    import Navbar to Home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React , { Fragment } from "react";
+import { Navbar } from '../../components'
+function Home() {
+    return (
+        <Fragment>
+            <header>
+                <div className="head-page">
+                    <Navbar />
+                </div>
+            </header>
+        </Fragment>
+    ) 
+    
+}
