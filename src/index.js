33cbc9a1c75d43afcc117bc9e39758ee2c431997
Mehdi <mehdihormozi22@gmail.com>
2022-07-06 14:08:01 +0300

    import react-splide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@splidejs/react-splide/css';
 import './Styles/main.scss'
 import Router from './Router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +10,4 @@ root.render(
     <Router />
   </React.StrictMode>
 );
+
