4c0992099ec697e257de7e178441f4256ef5a5c8
Mehdi <mehdihormozi22@gmail.com>
2022-07-06 15:46:53 +0300

    change Request

--- src/Hook/Request.jsx
+++ src/Hook/Request.jsx
@@ -1,22 +1,22 @@
-export const useRequest = () => { 
-  const Request = async ({ url, form, method = 'POST' }) => { 
-   const body = {...form,re_password:form.password } 
-   console.log(body) 
-    return await fetch('https://verifybymail-cors.herokuapp.com/auth/users/', { 
-      method: method, 
-      body: body 
-    }).then((resposen) => { 
-      if (resposen.status === 200) { 
-        return resposen.json().then((res) => { return { result: res, status: true } }); 
-      } else { 
-        return resposen.json().then((result) => { 
-          return { result, status: false } 
-        }) 
-      } 
-    }).catch((error) => { 
-      return { status: false, result: { message: 'error network!' } } 
-    }) 
- 
-  } 
-  return { Request } 
+
+export const useRequest = () => {
+  const Request = async ({ url, form, method = 'POST' }) => {
+    return await fetch('https://verifybymail-cors.herokuapp.com/' + url, {
+      method: method,
+      body: JSON.stringify(form)
+    }).then((resposen) => {
+      if (resposen.status === 200) {
+        return resposen.json().then((res) => { return { result: res, status: true } });
+      } else {
+        return resposen.json().then((result) => {
+          return { result, status: false }
+        })
+      }
+    }).catch((error) => {
+      return { status: false, result: { message: 'error network!' } }
+    })
+
+  }
+  return { Request }
 }
+
